c162c51cc80ceb698f2e38f8fe430da3f4cc7b99
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-13 21:45:47 -0700

    add SplitString method

--- SplitStrings.js
+++ SplitStrings.js
@@ -1,7 +1,10 @@
 function solution(str){
     let newArr = [];
+    if (str.length % 2 != 0) {
+        str = str.concat("_");
+    }  
     for (let i = 0; i < str.length; i+=2) {
-       newArr.push(str[i]+(str[i+1]));
-     }
+        newArr.push(str[i]+(str[i+1]));
+    }
     return newArr;
   }
